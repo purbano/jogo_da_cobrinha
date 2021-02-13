6260f72f6842624f8f92f177256225faa6e1f25a
purbano <purbano.us@gmail.com>
2021-02-12 21:38:31 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //Renderiza o desenho do canvas
 let box = 32; //definição dos pixels
+let snake =[]
+snake[0] = {
+    x: 8*box,
+    y: 8*box
+}
 
 //Função inicial do Canvas
 function criarBG(){
@@ -8,4 +13,12 @@ function criarBG(){
     context.fillRect(0, 0, 16*box, 16*box); //Desenho do retângulo em que ocorrerá o jogo (x, y, altura e largura)
 }
 
+function criarCobrinha(){
+    for(i=0; i< snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
