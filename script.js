2d8f272bd5d13227a785df15261bfedd4e65d686
purbano <purbano.us@gmail.com>
2021-02-13 07:19:51 -0300

    definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -23,18 +23,37 @@ function criarCobrinha(){
     }
 }
 
-function iniciarJogo(){    
+//Atribuindo funcionalidades aos botões de comando
+document.addEventListener('keydown', update) //keydown → clicar no botão
+
+function update (event){ //Direção dos comandos com uma condição de não voltar para o lado oposto
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){ 
+    //Lógica para que a cobrinha ao ultrapassar o canvas apareça do outro lado
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0
+    if(snake[0].x < 0*box && direction == "left") snake[0].x = 15
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0
+    if(snake[0].y < 0*box && direction == "up") snake[0].y = 15
+
     criarBG();
     criarCobrinha();
 
+    //posições iniciais da cobrinha
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    //Direções da cobrinha
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeX += box;
+    if(direction == "down") snakeY += box;
 
+    //Função para aumentar a cobrinha
     snake.pop();
 
     let newHead = {
@@ -42,7 +61,7 @@ function iniciarJogo(){
         y: snakeY
     }
 
-        snake.unshift(newHead)
+        snake.unshift(newHead);
 }
 
 let jogo = setInterval(iniciarJogo, 100); //a cada 100 ms renova o jogo
